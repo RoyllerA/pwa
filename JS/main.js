43fcc9745d85f8994c7aebe9b0382da02d277df7
RoyllerA <103551144+RoyllerA@users.noreply.github.com>
2022-04-13 10:06:22 -0500

    Add files via upload

---
+++ JS/main.js
@@ -0,0 +1,11 @@
+window.onload = () => {
+    'use strict';
+  
+    if ('serviceWorker' in navigator) {
+      navigator.serviceWorker
+               .register('./sw.js')
+               .then(function(){console.log('Service Worker Registered');});
+               
+    }
+  }
+  
